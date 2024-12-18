00216be75a4bba2743b47df83c01bf23c9bc9a06
Angel Zuniga <angel.zuna@icloud.com>
2024-12-18 13:34:47 -0500

    change port options and added run script

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import helmet from "helmet";
 import { helmetConfig } from "./helmetConfig";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(helmet(helmetConfig));
 app.use(express.json());
 
